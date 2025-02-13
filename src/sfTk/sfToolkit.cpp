09c3f2d35770903045571104c517dd50f9d4136e
Kirk Benell <kirk.benell@sparkfun.com>
2025-02-13 16:48:30 -0700

    update file headers w/ doxygen comments, modern license declaration

--- src/sfTk/sfToolkit.cpp
+++ src/sfTk/sfToolkit.cpp
@@ -1,3 +1,17 @@
+/**
+ * @file sfToolkit.cpp
+ * @brief Implementation file for the SparkFun Toolkit.
+ *
+ * This file contains the implementation of various utility functions
+ * for the SparkFun Toolkit library.
+ *
+ * @author SparkFun Electronics
+ * @date 2024-2025
+ * @copyright Copyright (c) 2024-2025, SparkFun Electronics Inc. This project is released under the MIT License.
+ * 
+ * SPDX-License-Identifier: MIT
+ */
+
 // File: sfToolkit.cpp
 //
 // General impl file for the SparkFun Toolkit
