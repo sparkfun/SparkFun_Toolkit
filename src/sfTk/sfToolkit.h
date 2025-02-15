c8754d6b2704e4abd8f54f06d9496eef03452337
Kirk Benell <kirk.benell@sparkfun.com>
2025-02-15 16:50:05 -0700

    added helpful bit macros

--- src/sfTk/sfToolkit.h
+++ src/sfTk/sfToolkit.h
@@ -48,3 +48,25 @@ void sftk_delay_ms(uint32_t ms);
 
 // ticks in milliseconds
 uint32_t sftk_ticks_ms(void);
+
+/**
+ * @brief Check if specific bits are set in a value.
+ *
+ * This macro checks if the bits specified by the bitmask are set in the given value.
+ *
+ * @param __value__ The value to check.
+ * @param __bitmask__ The bitmask specifying which bits to check.
+ * @return True if all bits specified by the bitmask are set, false otherwise.
+ */.
+#define SFTK_CHECK_BITS_SET(__value__, __bitmask__) (((__value__) & (__bitmask__)) == __bitmask__)
+
+/**
+ * @brief Check if specific bits are cleared in a value.
+ *
+ * This macro checks if the bits specified by the bitmask are cleared in the given value.
+ *
+ * @param __value__ The value to check.
+ * @param __bitmask__ The bitmask specifying which bits to check.
+ * @return True if all bits specified by the bitmask are cleared, false otherwise.
+ */
+#define SFTK_CHECK_BITS_CLEARED(__value__, __bitmask__) (((__value__) & (__bitmask__)) == 0)
